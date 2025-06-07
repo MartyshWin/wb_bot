0888d4fd55d228429876ceb905cc32fcfe092733
MartyshWin <86982485+MartyshWin@users.noreply.github.com>
2025-06-07 15:21:01 +0500

    Добавление новых обработчиков

--- app/routes/callbacks/general.py
+++ app/routes/callbacks/general.py
@@ -1,5 +1,7 @@
 from aiogram import Router, Bot, F
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
+import logging
 
 from app.commons.responses.general import GeneralResponse
 from app.keyboards.inline.general import InlineKeyboardHandler
@@ -9,17 +11,18 @@ controller = GeneralResponse()
 inline = InlineKeyboardHandler()
 
 
-@router.callback_query(F.data == 'home')
-async def main(callback_query: CallbackQuery) -> None | dict:
+@router.callback_query(F.data == 'main')
+async def main(callback_query: CallbackQuery, state: FSMContext) -> None | dict:
     """
     Обработчик кнопки "Главное меню" или "Назад".
 
     :param callback_query: Объект, содержащий данные о взаимодействии пользователя с кнопкой.
     """
+    await state.clear()
     try:
         user_lang = callback_query.from_user.language_code or "unknown"
-        response = controller.start_command_response(user_lang, callback_query.from_user.full_name)
+        response = await controller.start_command_response(callback_query.from_user.id, callback_query.from_user.username, user_lang)
 
         await callback_query.message.edit_text(response.text, reply_markup=inline.get_keyboard(response.kb))
     except Exception as e:
-        return {'logs_error': str(e)}
+        logging.error("message:" + str(e))
